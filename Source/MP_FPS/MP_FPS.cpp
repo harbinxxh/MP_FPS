283b295382e14a2bc7694dc4bf9eb88ee664d11d
xuxh <harbinxxh@gmail.com>
2026-09-23 15:47:46 +0800

    Unreal Engine 5.8.3 Project Setup

---
+++ Source/MP_FPS/MP_FPS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MP_FPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MP_FPS, "MP_FPS" );
